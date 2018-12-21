20f29ca255b4a354251d75e3ac0ba4c6c2d21abf
Kevin Atteson <kevin@atteson.com>
2018-12-21 06:59:31 -0500

    convolve seems to work

--- src/DisplacementRank.jl
+++ src/DisplacementRank.jl
@@ -1,5 +1,7 @@
 module DisplacementRank
 
+using FFTW
+
 abstract type DisplacementRankMatrix{T <: Number} end
 
 mutable struct ToeplitzMatrix{T} <: DisplacementRankMatrix{T}
@@ -7,6 +9,30 @@ mutable struct ToeplitzMatrix{T} <: DisplacementRankMatrix{T}
 end
 
 Base.getindex( M::ToeplitzMatrix{T}, i::U, j::U ) where { T, U <: Integer } =
-    M.entries[div(length(M.entries)+1,2) + i - j]    
+    M.entries[div(length(M.entries)+1,2) + i - j]
+
+function convolve( u::AbstractVector{T}, v::AbstractVector{T} ) where {T <: Number}
+    n = length(u)
+    m = length(v)
+    h = n + m - 1
+    N = 2^Int(ceil(log2(h)))
+    z = zero(T)
+    
+    resize!( u, N )
+    for i = n+1:N
+        u[i] = z
+    end
+    
+    resize!( v, N )
+    for i = m+1:N
+        v[i] = z
+    end
+
+    fu = rfft(u)
+    fv = rfft(v)
+
+    w = irfft(fu .* fv, N)
+    return w[1:h]
+end
 
 end # module
--- test/test.jl
+++ test/test.jl
@@ -1,1 +1,20 @@
 using DisplacementRank
+using Random
+
+Random.seed!(1)
+u = rand(3)
+v = rand(7)
+
+w = DisplacementRank.convolve(u, v)
+
+w2 = zeros(9)
+for i = 1:9
+    for j = 1:3
+        k = i - j + 1
+        if 0 < k < 8
+            w2[i] += u[j] * v[k]
+        end
+    end
+end
+
+@assert( maximum(abs.(w2 - w)) < 1e-10 )
